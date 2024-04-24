e41bc1ffa75e79816c612a77cf63f965af5bb097
Kajaltiwari88 <98203922+Kajaltiwari88@users.noreply.github.com>
2024-04-24 23:03:32 +0530

    Update index.jsx

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -5,8 +5,69 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="relative justify-normal mx-14 my-14 ">
-      <div className="flex flex-col justify-center ">
+    <div className="relative mx-14 my-14">
+      <div className="flex flex-col lg:flex-row justify-center items-center">
+        <div className="lg:hidden w-full">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateY(-1200px)",
+            }}
+            end={{
+              transform: "translateY(0px)",
+            }}
+          >
+            <div className="w-full">
+              <img className="border-black rounded-3xl" src={img1} alt="img1" />
+            </div>
+          </Animate>
+        </div>
+        
+        <div className="lg:w-[45%] w-[100%] mr-auto">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-1200px)",
+            }}
+            end={{
+              transform: "translateX(0px)",
+            }}
+          >
+            <h1 className="lg:leading-relaxed lg:text-3xl text-amber-300 font-semibold text-[1.5rem] lg:text-left text-center ">
+              Hi, I am Kajal Tiwari
+              <br />
+              A Beginner Web Developer
+            </h1>
+          </Animate>
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateY(900px)",
+            }}
+            end={{
+              transform: "translateY(0px)",
+            }}
+          >
+            <Link to={"/contact"} className="flex flex-col items-center mt-4 ">
+              <button
+                className="flex lg:absolute left-0 border border-red-800 rounded-lg text-[#f7f1e3] font-bold lg:text-2xl text-2xl bg-[#227093] w-35 py-2 px-3 cursor-pointer hover:bg-[#34ace0]"
+                style={{ margin: "auto" }} // Centering the button horizontally
+              >
+                Hire Me
+                <span className="pl-3 pt-2">
+                  <BsFillArrowRightSquareFill size={22} />
+                </span>
+              </button>
+            </Link>
+          </Animate>
+        </div>
+        
         <Animate
           play
           duration={1.5}
@@ -18,60 +79,14 @@ const Home = () => {
             transform: "translateX(0px)",
           }}
         >
-          <div className="lg:absolute top-42 right-0 lg:w-[40%] w-[100%]">
-            <img className="border-black rounded-3xl" src={img1} />
+          <div className="lg:w-[46%] w-[100%] ml-auto mt-10 lg:mt-10">
+            <img className="border-black rounded-3xl hidden lg:block" src={img1} alt="img1" />
           </div>
         </Animate>
-
-        <Animate
-          play
-          duration={1.5}
-          delay={1}
-          start={{
-            transform: "translateX(-1200px)",
-          }}
-          end={{
-            transform: "translateX(0px)",
-          }}
-        >
-          <h1
-            className="lg:leading-relaxed lg:text-5xl text-amber-300
-           font-semibold text-[1.5rem] lg:text-left text-center mt-5"
-          >
-            Hi,I am Kajal Tiwari
-            <br />A Beginner Web Developer
-          </h1>
-        </Animate>
-       
-          <Animate
-            play
-            duration={1.5}
-            delay={1}
-            start={{
-              transform: "translateY(900px)",
-            }}
-            end={{
-              transform: "translateX(80px)",
-            }}
-          >
-            <Link to={"/contact"}>
-            <button
-              className="flex items-center border border-red-800 rounded-lg 
-            text-[#f7f1e3] font-bold lg:text-2xl bg-[#227093]
-          w-38 py-2 px-3 mt-6 cursor-pointer hover:bg-[#34ace0] "
-            >
-               Hire Me
-             
-              <span className="pl-3 justify-center py-1">
-                <BsFillArrowRightSquareFill size={24} />
-              </span>
-            </button>
-            </Link>
-          </Animate>
-        </div>
       </div>
-
+    </div>
   );
 };
 
 export default Home;
+
